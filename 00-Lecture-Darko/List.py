902324116b917f0990d1977ce4fc415ef0216f99
darkovelkov <velkovd@gmail.com>
2024-02-15 22:20:58 +0100

    Adding the py script where I've experimented with Lists

---
+++ 00-Lecture-Darko/List.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 15 21:56:22 2024
+
+@author: user
+"""
+
+my_list=['one', 'two']
+my_list2=['three']
+my_list3= my_list+my_list2
+print(my_list3)
+# you can do slicing and concatenation as for Strings
+my_list3.append("add something here")
+my_list3.pop()
+my_list3.pop(1)
+new_list = ['a','s', 'c' ,'w']
+new_list
+new_list.sort()
+new_list.reverse()
+new_list
+
+#you can add and remove items in the list and the type does not to be the same
+#you can sort and unsort with reverse
