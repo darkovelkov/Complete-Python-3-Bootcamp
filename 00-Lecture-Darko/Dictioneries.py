671746ee05f5eb04ae8b5858dda8515dc314e442
Darko Velkov <darko.velkov@iwconnect.com>
2024-02-16 15:41:17 +0100

    adding files for Dic, tuplets and sets

---
+++ 00-Lecture-Darko/Dictioneries.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 13:53:20 2024
+
+@author: darko.velkov
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+my_dict= {'apple':2 ,'orange':2.50 ,'milk': 5.80 }
+my_dict['apple']
+my_dict.values()
---
+++ 00-Python Object and Data Structure Basics/Tuplets and Sets.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 16 13:57:06 2024
+
+@author: darko.velkov
+"""
+#this is how you define set and it must be with unique values -> you can not add list as a part of the set
+my_set = set([1,'pece'])
+
+type(my_set)
+
+my_tuple = (1,2,3,4)
+type(my_tuple)
+
+my_tuple.count(1)
