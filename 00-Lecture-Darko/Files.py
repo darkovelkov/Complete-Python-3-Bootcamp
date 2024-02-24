88d42daf441afd3e0644f947ebf277a1c8f44480
darkovelkov <velkovd@gmail.com>
2024-02-24 21:17:20 +0100

    This is a commit for IO on files

---
+++ 00-Lecture-Darko/Files.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 24 20:29:19 2024
+
+@author: user
+"""
+
+# This is a script for I/0 or writing and reading from a file
+
+#myfile = open('file.txt', 'w')
+#myfile.write('this is some text')
+#myfile.close()
+
+#test= open('file.txt' ,'r')
+#content = test.read()
+#print(content)
+
+with open('/home/user/file.txt', mode='r') as f:
+    print(f.readlines())
+    
