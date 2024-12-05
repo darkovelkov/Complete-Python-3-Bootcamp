60a529f665b3246ba48d14dfad3fe9c9e17a97de
Darko Velkov <darko.velkov@iwconnect.com>
2024-12-05 12:57:36 +0100

    Adding from work PC

---
+++ 07-Lecture-Darko/Simple.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 18 11:00:11 2024
+
+@author: darko.velkov
+"""
+
+def myfunc():
+    """
+    This is some simple dec    
+    """
+    first=1
+    second=2
+    print(first)
+    print(second)
+   
+myfunc()
